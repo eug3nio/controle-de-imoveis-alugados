2a88b868ba687a74b87802a601cbc682cd4a2088
eug3nio <eugenio.merlin@hotmail.com>
2022-12-01 20:17:23 -0300

    Criando os componentes necessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditImovelComponent } from './imovel/edit/edit.component';
+import { ListImovelComponent } from './imovel/list/list.component';
+
+import { EditLocacaoComponent } from './locacao/edit/edit.component';
+import { ListLocacaoComponent } from './locacao/list/list.component';
+
+import { EditLocatarioComponent } from './locatario/edit/edit.component';
+import { ListLocatarioComponent } from './locatario/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditImovelComponent,
+    ListImovelComponent,
+    EditLocacaoComponent,
+    ListLocacaoComponent,
+    EditLocatarioComponent,
+    ListLocatarioComponent
   ],
   imports: [
     BrowserModule,
