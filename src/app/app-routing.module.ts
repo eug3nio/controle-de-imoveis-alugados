0c9ad744d49bd9873cb2bc4d8ba939c2f7051af2
eug3nio <eugenio.merlin@hotmail.com>
2022-12-03 18:02:08 -0300

    Mapeando e fazendo o rotamento entre os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
+import { EditLocacaoComponent } from './locacao/edit/edit.component';
+import { ListImovelComponent } from './imovel/list/list.component';
+import { EditImovelComponent } from './imovel/edit/edit.component';
+import { EditLocatarioComponent } from './locatario/edit/edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListLocatarioComponent } from './locatario/list/list.component';
+import { ListLocacaoComponent } from './locacao/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'locatario/edit', component: EditLocatarioComponent },
+  { path: 'locatario/list', component: ListLocatarioComponent},
+  { path: 'imovel/edit', component: EditImovelComponent },
+  { path: 'imovel/list', component: ListImovelComponent },
+  { path: 'contrato/edit', component: EditLocacaoComponent },
+  { path: 'contrato/list', component: ListLocacaoComponent },
+  { path: '', redirectTo: 'locatario', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
